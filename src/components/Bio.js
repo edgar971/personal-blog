a3631dea5b27bb8078ce507d6c881a6fd80e8c23
Edgar Pino <edgarbeltran@me.com>
2018-09-19 16:37:49 -0600

    Setup blog

---
+++ src/components/Bio.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+// Import typefaces
+import 'typeface-montserrat'
+import 'typeface-merriweather'
+
+import profilePic from './profile-pic.jpg'
+import { rhythm } from '../utils/typography'
+
+class Bio extends React.Component {
+  render() {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          marginBottom: rhythm(2.5),
+        }}
+      >
+        <img
+          src={profilePic}
+          alt={`Edgar Pino`}
+          style={{
+            marginRight: rhythm(1 / 2),
+            marginBottom: 0,
+            borderRadius: '50%',
+            width: rhythm(2),
+            height: rhythm(2),
+          }}
+        />
+        <p>
+          Software Engineer @Pluralsight. Interested in distributed systems, machine learning, and the web. <a href="https://twitter.com/edgar971">Follow me on Twitter.</a>
+        </p>
+      </div>
+    )
+  }
+}
+
+export default Bio
