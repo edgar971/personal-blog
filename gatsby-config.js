9c789054680da96902f9878cbe177907b9bb1ad8
Edgar Pino <edgarbeltran@me.com>
2018-09-20 15:22:40 -0600

    Update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,8 +48,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Edgar Pino's Blog`,
+        short_name: `Edgar Pino`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
